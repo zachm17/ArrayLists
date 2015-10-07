e98c21a822212f89ed69e34faa1ce60b69c2a069
zachm17 <zachmoy17@gmail.com>
2015-10-07 09:56:02 -0600

    Made an ArrayList and a JOptionPane

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,10 +1,29 @@
 package lists.controller;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
 	
 	public void start()
 	{
-		
+		firstWords.add("randomadnjafn admdamafj");
+		firstWords.add("babababbaba");
+		showList();
+	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) + " is at spot# " + spot);
+		}
 	}
 }
